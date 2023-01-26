bd16036484b279671dcf2e2444ba12597cd79bab
Randall <randms07@hotmail.com>
2023-01-25 20:52:08 -0600

    add comments to the game class code. A game class object with It's RunGame function has been called within the main function.

--- Game.cs
+++ Game.cs
@@ -6,32 +6,37 @@
         {
             //init variables
             int attemps = 1;
-            int userNumber = this.validateNumber(Console.ReadLine());
+
             //random number between 0 and 9999
             int secretNumber = new Random().Next(0, 9999);
+
+            Console.WriteLine("-Enter a integer number to start guessing");
+            int userNumber = this.validateNumber(Console.ReadLine());
 
             //main while cicle. It will ask for a number while user's number is different to the secret number.
             while (userNumber != secretNumber)
             {
-                //is the user number the secret number??? smaller or larger?
-                //if smaller then display is too small
+                /*is the user's number the secret number??? smaller or larger?
+                  if smaller then display is too small
+                */
                 if (userNumber < secretNumber)
                 {
-                    Console.WriteLine("Number is too small");
+                    Console.WriteLine("Tip: Number is too small!");
                 }
                 //if larger then display is too large
                 else
                 {
-                    Console.WriteLine("Number is too large");
+                    Console.WriteLine("Tip: Number is too large!");
                 }
                 //count the attemps of the user when guessing!
                 attemps += 1;
-                //again, whe need a valid integer number for the guessing.
+                /*again, whe need a valid integer number for the guessing.
+                  The try will count only if number is valid. Because if number is valid that means you have tried for real.
+                */
                 userNumber = validateNumber(Console.ReadLine());
             }
-
             Console.WriteLine("Congrats! You have guessed the number with " + attemps + " attemps.");
-            Console.WriteLine("***THANK YOU!. END OF THE GAME***");
+            Console.WriteLine("***THANK YOU FOR THE OPPORTUNITY!. END OF THE GAME***");
         }
 
         //this method basically ask if number is valid.
--- Program.cs
+++ Program.cs
@@ -1,9 +1,13 @@
-﻿class Program
+﻿using GuessingGame;
+
+class Program
 {
 
     static void Main(string[] args)
     {
-
+        Console.WriteLine("Welcome to the guessing game!");
+        Game game = new Game();
+        game.RunGame();
         return;
     }
 
